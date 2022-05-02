ac106dab3471dd94a10e99746b000501ba2e19e7
Brunarquino <brunarquino@gmail.com>
2022-05-02 16:03:29 -0300

    Exercicios do while

--- src/dio/estruturaDeRepeticoes/MaiorMedia.java
+++ src/dio/estruturaDeRepeticoes/MaiorMedia.java
@@ -1,4 +1,47 @@
 package dio.estruturaDeRepeticoes;
 
+import java.util.Scanner;
+
 public class MaiorMedia {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int numero;
+        int maior = 0;
+        String todosNumeros = "";
+        int media = 0;
+
+        int count = 0;
+
+        do {
+            //Pedir o numero 5 vezes
+            System.out.println("Número: ");
+            numero = scan.nextInt();
+
+            //Montando uma string com todos números
+            todosNumeros += numero;
+            if(count < 4) {
+                todosNumeros += " + ";
+            }
+            count++;
+
+            // Motando a media
+            media += numero;
+            if(count == 5) {
+                media = media/5;
+            }
+            // Se o numero atual for maior que o numero de maior, o numnero se torna o maor numero
+            if(numero > maior) maior = numero;
+
+            System.out.println("Maior Número Atual: " + maior);
+        }while (count < 5);
+
+
+        System.out.println("Maior Número Final: " + maior);
+
+        System.out.println("Média dos números " + todosNumeros + " é " + media);
+
+    }
+
 }
--- src/dio/estruturaDeRepeticoes/ParEImpar.java
+++ src/dio/estruturaDeRepeticoes/ParEImpar.java
@@ -1,4 +1,38 @@
 package dio.estruturaDeRepeticoes;
 
+import java.util.Scanner;
+
 public class ParEImpar {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int quantNumeros;
+        int numero;
+        int quantPares = 0, quantImpares = 0;
+
+        // pede quantos numeros quer colocar
+        System.out.println("Quantidade de números: ");
+        quantNumeros = scan.nextInt();
+
+        int count = 0;
+        do {
+            //pede os números
+            System.out.println("Número: ");
+            numero = scan.nextInt();
+
+            // se o resto da divisão for igual a zero numero é par
+            if(numero % 2 == 0){
+                quantPares++;
+            }else{ //se não é impar
+                quantImpares++;
+            }
+            count++;
+
+        }while (count < quantNumeros);
+
+        System.out.println("Quantidade Par: " + quantPares);
+        System.out.println("quantidade Imapar: " + quantImpares);
+
+
+    }
 }
