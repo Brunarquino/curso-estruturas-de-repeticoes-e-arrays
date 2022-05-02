ac488a499fa02b45dded0cd055aa2657b0eb3408
Brunarquino <brunarquino@gmail.com>
2022-05-02 16:24:48 -0300

    Exercicios do + exercicios

--- src/dio/estruturaDeRepeticoes/Tabuada.java
+++ src/dio/estruturaDeRepeticoes/Tabuada.java
@@ -1,4 +1,53 @@
 package dio.estruturaDeRepeticoes;
 
+import java.util.Scanner;
+
 public class Tabuada {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int tabuada;
+        int multiplicacao;
+
+        System.out.println("Escolha a tabuada de  1 á 10? ");
+        tabuada = scan.nextInt();
+
+        while (tabuada < 1 | tabuada > 10){
+
+            System.out.println("Numero Invalido! Tente Novamente! \nEscolha a tabuada de  1 á 10? ");
+            tabuada = scan.nextInt();
+
+        }
+
+        //tabuada com for
+
+        System.out.println("Tabuada do " +  tabuada + ": ");
+        for (int i = 1; i <= 10; i++){
+            multiplicacao = tabuada * i;
+            System.out.println(tabuada + " x " + i + " = " + multiplicacao);
+        }
+
+        //tabuada com do while
+
+        int count1 = 1;
+        System.out.println("\nTabuada do " +  tabuada + ": ");
+        do{
+            multiplicacao = tabuada * count1;
+            System.out.println(tabuada + " x " + count1 + " = " + multiplicacao);
+            count1++;
+        }while (count1 <= 10);
+
+
+        //tabuada com while
+
+        int count2 = 0;
+        System.out.println("\nTabuada do " +  tabuada + ": ");
+        while (count2 <= 10){
+            multiplicacao = tabuada * count2;
+            System.out.println(tabuada + " x " + count2 + " = " + multiplicacao);
+            count2++;
+        };
+
+
+    }
 }
