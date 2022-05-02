a36a42bf687d483403394e918b11a414b3663d70
Brunarquino <brunarquino@gmail.com>
2022-05-02 16:46:20 -0300

    Exercicios de for

--- src/dio/estruturaDeRepeticoes/Fatorial.java
+++ src/dio/estruturaDeRepeticoes/Fatorial.java
@@ -1,4 +1,24 @@
 package dio.estruturaDeRepeticoes;
 
+import java.util.Scanner;
+
 public class Fatorial {
+    public static void main(String[] args) {
+        //Programa que calcula o Fatorial de um número inteiro fornecido pelo usúario
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Fatorial: ");
+        int fatorial = scan.nextInt();
+
+        int multiplicacao = 1;
+
+        System.out.println(fatorial + "! = ");
+        for(int i = fatorial; i >= 1; i--) {
+            multiplicacao = multiplicacao * i;
+
+        }
+
+        System.out.println(multiplicacao);
+
+    }
 }
